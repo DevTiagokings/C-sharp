df57edf4644c704641e23c68785e55b73ec6c106
Tiago Gomes do Nascimento <Gomes.gt001@gmail.com>
2024-09-12 16:49:10 -0300

    Desenvolvimento c#

---
+++ ProjetoClientes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace ProjetoClientes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        try{
+                //executar
+                SqlConnectionStringBuilder builder=new SqlConnectionStringBuilder(
+                    "User id=sa;Password=Gomes0906;"+
+                    "Database=projetoclientes;"
+
+                );
+
+
+        }catch(Exception e) 
+        {
+                //exception
+                System.Console.WriteLine("Erro:" + e.ToString());
+
+        }
+    }
+}
