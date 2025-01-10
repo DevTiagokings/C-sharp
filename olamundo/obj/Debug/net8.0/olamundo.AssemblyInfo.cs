ca839247cbc580db1344ae0219d239874a91903d
Tiago Gomes do Nascimento <Gomes.gt001@gmail.com>
2025-01-10 11:08:59 -0300

    atualização

--- olamundo/obj/Debug/net8.0/olamundo.AssemblyInfo.cs
+++ olamundo/obj/Debug/net8.0/olamundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("olamundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a43d28043bff3548c5fee45e9c2126b70acfa70")]
 [assembly: System.Reflection.AssemblyProductAttribute("olamundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("olamundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
